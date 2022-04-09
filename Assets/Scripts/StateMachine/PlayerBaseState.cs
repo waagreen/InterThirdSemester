05ab20eddf998c8c09b8f0de3104cf80ae6092a5
Danniells <danniells.bisaggio@gmail.com>
2022-04-09 15:26:55 -0300

    no message

---
+++ Assets/Scripts/StateMachine/PlayerBaseState.cs
@@ -0,0 +1,25 @@
+public abstract class PlayerBaseState
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /* void EnterState();
+
+    void UpdateState();
+
+    void ExitState();
+
+    void CheckSwitchStates();
+
+    void InitializeSubState();
+    */
+}
