e688290f0f27fc5f78df6f9c1043354f39bb15ff
DESKTOP-61K197E\wagren <silvacostawagner@gmail.com>
2022-04-20 19:38:28 -0300

    stress minigame half done

--- Assets/Scripts/System/UiSystem.cs
+++ Assets/Scripts/System/UiSystem.cs
@@ -11,7 +11,6 @@ public class UiSystem : MonoBehaviour
     public BarData stress;
 
     public RectTransform point;
-    private List<RectTransform> points = new List<RectTransform>();
 
     private bool isStressed = false;
     private BarData activeBar;
@@ -20,23 +19,25 @@ public class UiSystem : MonoBehaviour
     {
         if (isStressed) return;
 
-        if (stressLevel >= 0.75f) activeBar = stress.BarSetup(barHolder, Color.red, 5f, 5);
-        else if (stressLevel >= 0.50f) activeBar = stress.BarSetup(barHolder, Color.yellow, 15f, 4);
-        else if (stressLevel >= 0.25f) activeBar = stress.BarSetup(barHolder, Color.blue, 20f, 3);
-        else activeBar = stress.BarSetup(barHolder, Color.cyan, 5f, 3);
+        if (stressLevel >= 0.75f) activeBar = stress.BarSetup(barHolder, Color.red, 3f, 5);
+        else if (stressLevel >= 0.50f) activeBar = stress.BarSetup(barHolder, Color.yellow, 7f, 4);
+        else if (stressLevel >= 0.25f) activeBar = stress.BarSetup(barHolder, Color.blue, 15f, 3);
+        else activeBar = stress.BarSetup(barHolder, Color.cyan, 20f, 3);
 
-        CreatePoints(activeBar.nPoints);
+        CreatePoints(activeBar.nPoints, activeBar.GetRandomRectWidth());
         isStressed = true;
     }
 
 
-    public void CreatePoints(int nPoints)
+    public void CreatePoints(int nPoints, float nWidth)
     {
         for (int i = 0; i < nPoints; i++)
         {
             Debug.Log($"Instaciate {i} bars: ");
-            var a = Instantiate(point, new Vector3(Random.Range(activeBar.startPos.position.x, activeBar.endPos.position.x), 0.5f, 0f), transform.rotation, activeBar.transform);
-            a.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, activeBar.barTransform.rect.x);
+            var a = Instantiate(point, new Vector3(Random.Range(activeBar.startPos.position.x, activeBar.endPos.position.x), 0.5f, 0f), transform.rotation, activeBar.barTransform.transform);
+            a.sizeDelta = new Vector2(nWidth, 0);
+            a.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0f, activeBar.barTransform.rect.size.y * 2);
+            a.SetAsFirstSibling();
         }
     }
 }
